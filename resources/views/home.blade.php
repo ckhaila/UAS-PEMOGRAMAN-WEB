eb520f142799a20b382eb54eb1c881c50fea1799
ckhaila <143923602+ckhaila@users.noreply.github.com>
2025-01-06 14:15:28 +0700

    Create home.blade.php

---
+++ resources/views/home.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"><img src="Selamat Datang.jpg" alt="Gambar Sorgum">
+    <title>Budidaya Sorgum</title>
+</head>
+<body></h1>
+    <h1>Daftar Petani</h1>
+    
+            <p>Nama:<p>HP: <p>Daerah: <p>DATA BUDIDAYA<p>• Lokasi Lahan<p>
+	• Luas Lahan (m²)<p>
+	• Jenis Benih<p>
+	• Tanggal Penanaman<p>
+	• Kondisi Tanah (pH, kelembapan)<p>
+	• Riwayat Pemupukan<p>
+	• Data Panen (Tanggal, Berat Panen)<h1>Daftar Petani</h1>
+    
+            <p>Nama:<p>HP: <p>Daerah: <p>DATA BUDIDAYA<p>• Lokasi Lahan<p>
+	• Luas Lahan (m²)<p>
+	• Jenis Benih<p>
+	• Tanggal Penanaman<p>
+	• Kondisi Tanah (pH, kelembapan)<p>
+	• Riwayat Pemupukan<p>
+	• Data Panen (Tanggal, Berat Panen)
+</h1>
+    <h2>LAPORAN</h2>
+    <p>Nama:<p>HP: <p>Daerah: <p>INFORMASI<p>• Peta Lokasi Lahan<p>
+	• Laporan Budidaya:<p>
+	• Data Pertumbuhan Tanaman<p>
+	• Grafik Hasil Panen<p>
+	• Riwayat Pemupukan dan Perawatan<p><h2>LAPORAN</h2>
+    <p>Nama:<p>HP: <p>Daerah: <p>INFORMASI<p>• Peta Lokasi Lahan<p>
+	• Laporan Budidaya:<p>
+	• Data Pertumbuhan Tanaman<p>
+	• Grafik Hasil Panen<p>
+	• Riwayat Pemupukan dan Perawatan<p>
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/tim-developer">Tim Developer</a></li>
+            <li><a href="/kontak">Kontak</a></li>
+        </ul>
+    </nav>
+</body>
+</html>
+</html>
