36564a1eb922369d9d527733087fc26fd018b1be
ckhaila <143923602+ckhaila@users.noreply.github.com>
2025-01-06 14:18:45 +0700

    Create tim-developer.blade.php

---
+++ resources/views/tim-developer.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tim Developer</title>
+</head>
+<body>
+    <h1>Tim Developer</h1>
+    <h2>Ketua:</h2>
+    <p>Nama: Cut Khaila Tiara Putri</p>NIM: 1234567<p>
+    <p>Tugas: Pengembangan Sistem</p>
+    
+    <h3>Anggota:</h3>
+    <ul>
+        <li>
+            <p>Nama: Cut Khaila<p>NIM: 2345678<p>Tugas: Desain Web<p>
+        </li>
+        <li>
+            <p>Nama: Cut Khaila<p>NIM: 2345678<p>Tugas: Pengujian Aplikasi
+        </li>
+        <li>
+             <p>Nama: Cut Khaila<p>NIM: 2345678<p>Tugas: Database
+        </li>
+    </ul>
+</body>
+</html>
