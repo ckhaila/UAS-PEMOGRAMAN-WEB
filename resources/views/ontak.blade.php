f208c8045ef1c91b92b797a14d45932ce3258127
ckhaila <143923602+ckhaila@users.noreply.github.com>
2025-01-06 14:17:15 +0700

    Create ontak.blade.php

---
+++ resources/views/ontak.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kontak</title>
+</head>
+<body>
+    <h1>Kontak Kami</h1>
+    <p>Email: ckhaila0502@gmail.com</p>
+    <p>Telepon: 082114333057</p>
+    <img src="8.jpg" alt="Gambar Sorgum">
+</body>
+</html>
